13cbfca8672ba72473546af7461065e3195b2364
Aniket Shah <annishah087@gmail.com>
2026-01-06 20:29:23 +0530

    01: project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const x = () =>{
+  return(
+    <div>
+      <Button> 
+        click me
+      </Button>
+    </div>
+  );
+};
+
+export default x;
